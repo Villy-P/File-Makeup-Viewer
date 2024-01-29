3c9a84bb8a1b02ed69a3daac8a3ff97cf7307b04
Villy-P <valeriuspetrini@gmail.com>
2024-01-29 15:27:29 -0600

    Send File to Webview

--- src/extension.ts
+++ src/extension.ts
@@ -1,12 +1,17 @@
 import { commands, ExtensionContext } from "vscode";
 import { HelloWorldPanel } from "./panels/HelloWorldPanel";
 import * as vscode from 'vscode'
+import path = require('path')
+import fs = require('fs')
 
 export function activate(context: ExtensionContext) {
     const showHelloWorldCommand = commands.registerCommand("file-makeup-viewer.show-makeup", () => {
 		HelloWorldPanel.render(context.extensionUri);
 
-		HelloWorldPanel.currentPanel?.post({title: "cwd", msg: vscode.workspace.workspaceFolders![0].uri.fsPath})
+		const FILE_JSON = vscode.Uri.file(path.join(context.extensionPath, "src", 'file.json'));
+
+		HelloWorldPanel.currentPanel?.post({title: "cwd", msg: vscode.workspace.workspaceFolders![0].uri.fsPath});
+		HelloWorldPanel.currentPanel?.post({title: "file", msg: fs.readFileSync(FILE_JSON.fsPath, 'utf8')})
     });
 
 	context.subscriptions.push(showHelloWorldCommand);
