ff446d146e3925546538e5ce80de9c85d249414d
Villy-P <valeriuspetrini@gmail.com>
2024-08-31 18:46:10 -0500

    chore: move files around

--- src/extension/extension.ts
+++ src/extension/extension.ts
@@ -1,25 +1,25 @@
 import { commands, ExtensionContext } from "vscode";
-import { HelloWorldPanel } from "./panels/HelloWorldPanel";
+import { SveltePanel } from "./panel";
 import * as vscode from 'vscode'
-import path = require('path')
-import fs = require('fs')
+import path from 'path';
+import fs from 'fs';
 import dirTree from 'directory-tree'
 
 function setupExtension(context: ExtensionContext) {
 	const FILE_JSON = vscode.Uri.file(path.join(context.extensionPath, "src", 'file.json'));
 	const cwd = vscode.workspace.workspaceFolders![0].uri.fsPath;
 
-	HelloWorldPanel.currentPanel?.post({title: "cwd",  msg: cwd});
-	HelloWorldPanel.currentPanel?.post({title: "file", msg: fs.readFileSync(FILE_JSON.fsPath, 'utf8')});
-	HelloWorldPanel.currentPanel?.post({title: "dir",  msg: JSON.stringify(dirTree(cwd, {attributes: [
+	SveltePanel.currentPanel?.post({title: "cwd",  msg: cwd});
+	SveltePanel.currentPanel?.post({title: "file", msg: fs.readFileSync(FILE_JSON.fsPath, 'utf8')});
+	SveltePanel.currentPanel?.post({title: "dir",  msg: JSON.stringify(dirTree(cwd, {attributes: [
 		"size", "type"
 	]}))});
 }
 
 export function activate(context: ExtensionContext) {
     const showHelloWorldCommand = commands.registerCommand("file-makeup-viewer.show-makeup", () => {
-		HelloWorldPanel.render(context.extensionUri);
-		HelloWorldPanel.currentPanel?._panel.onDidChangeViewState(e => {
+		SveltePanel.render('showPanel', 'File Makeup', context.extensionUri);
+		SveltePanel.currentPanel?.panel.onDidChangeViewState(e => {
 			setupExtension(context);
 		}, null, context.subscriptions)
 
